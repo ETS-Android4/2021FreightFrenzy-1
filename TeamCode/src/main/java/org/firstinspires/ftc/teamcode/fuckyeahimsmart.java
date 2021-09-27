d0cc1209d05def35259a712fce04d9b7d2b70195
RoboDilbert <Knackrobotics@gmail.com>
2021-09-26 19:52:35 -0500

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fuckyeahimsmart.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Ben sucks fat cock trueee", group="Linear Opmode")
+
+public class fuckyeahimsmart extends LinearOpMode{
+    public DcMotor benGay = null;
+
+    public void runOpMode() {
+
+        benGay = hardwareMap.get(DcMotor.class, "benGay");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            benGay.setPower(gamepad1.left_stick_y);
+
+        }
+
+    }
+}
