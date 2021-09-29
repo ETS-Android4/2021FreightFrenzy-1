c3fd483e8762efe81759dfc03059d351cb0433bd
RoboDilbert <Knackrobotics@gmail.com>
2021-09-28 20:00:30 -0500

    end of meeting 9/28/21

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fuckyeahimsmart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fuckyeahimsmart.java
@@ -4,25 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Ben sucks fat cock trueee", group="Linear Opmode")
+@TeleOp(name="Push Bot Test", group="Linear Opmode")
 
 public class fuckyeahimsmart extends LinearOpMode{
-    public DcMotor benGay = null;
+    public DcMotor frontMotor;
+    public DcMotor backMotor;
 
     public void runOpMode() {
 
-        benGay = hardwareMap.get(DcMotor.class, "benGay");
+        frontMotor = hardwareMap.get(DcMotor.class, "frontMotor");
+        backMotor = hardwareMap.get(DcMotor.class, "backMotor");
+
+        backMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while(opModeIsActive()){
 
-            benGay.setPower(gamepad1.left_stick_y);
-
-            benGay.setPower(69);
+            frontMotor.setPower(gamepad1.right_stick_y);
+            backMotor.setPower(gamepad1.left_stick_y);
 
         }
 
