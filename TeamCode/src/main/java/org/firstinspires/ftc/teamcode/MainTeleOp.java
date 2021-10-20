6d3bbdf107fcdfedcd196d3e00e47d9d09c65c34
RoboDilbert <Knackrobotics@gmail.com>
2021-10-19 20:01:18 -0500

    10/19/21 - End of meeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.Carousel;
 
 @TeleOp(name="Push Bot Test", group="Linear Opmode")
 
@@ -11,14 +12,37 @@ public class MainTeleOp extends LinearOpMode{
 
     public void runOpMode() throws InterruptedException {
         DriveTrain.initDriveTrain(hardwareMap);
+        Carousel.initCarousel(hardwareMap);
 
         waitForStart();
 
+        boolean adjust = false;
         while(opModeIsActive()){
 
             DriveTrain.leftFront.setPower(gamepad1.left_stick_y);
             DriveTrain.rightFront.setPower(gamepad1.right_stick_y);
 
+            if(gamepad2.x){
+                adjust = !adjust;
+            }
+
+            if(gamepad2.y) {
+                while(gamepad2.y){}
+                Carousel.adjustUp();
+            }else if(gamepad2.a) {
+                while(gamepad2.a){}
+                Carousel.adjustDown();
+            }
+
+            if(adjust) {
+                Carousel.powerMotor();
+            }else {
+                Carousel.accelerate(gamepad2.left_stick_y);
+            }
+
+            telemetry.addData("Adjust: ", adjust);
+            telemetry.addData("Carousel Power: ", Carousel.carouselPower);
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Carousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Carousel.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Carousel {
+    public static DcMotor carouselMotor;
+
+    public static double carouselPower = 0.0;
+
+    public Carousel(){};
+
+    public static void initCarousel(HardwareMap hwm){
+        //Declare Motors on hardware map
+        carouselMotor = hwm.get(DcMotor.class, "carouselMotor");
+    }
+
+    public static void accelerate(double yValue){
+         carouselMotor.setPower(Math.pow(yValue, 2));
+    }
+
+    public static void adjustUp(){
+        carouselPower += .01;
+    }
+
+    public static void adjustDown(){
+        carouselPower -= .01;
+    }
+
+    public static void powerMotor(){
+        carouselMotor.setPower(carouselPower);
+    }
 }
